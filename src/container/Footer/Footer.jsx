21ed9e3317e779bd9b9b934b932c6fa7a3facc9c
Nicole Soeng <nicole.soeng@outlook.com>
2022-08-10 15:00:34 -0400

    footer update

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => (
       </div>
     </div>
     <div className="footer__copyright">
-      <p className="p__opensans">2021 &copy; Noodish. All Rights Reserved.</p>
+      <p className="p__opensans">2022 &copy; Noodish. All Rights Reserved.</p>
     </div>
   </div>
 );
