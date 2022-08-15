f4b8224bb1b1738de8ea129474413ed0621eabad
Nicole Soeng <nicole.soeng@outlook.com>
2022-08-14 20:31:14 -0400

    updated files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 
 import { AboutUs, FindUs, Footer, Gallery, Header, Intro, SpecialMenu } from './container';
-// import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
   <div>
-    {/* <Navbar /> */}
     <Intro />
     <Header />
     <AboutUs />
     <SpecialMenu />
-    {/* <Chef /> */}
-
     <Gallery />
     <FindUs />
     <Footer />
   </div>
 );
 
-export default App;
+export default App; 
